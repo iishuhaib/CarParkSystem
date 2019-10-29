1ac94261181b883d3b4e91800af81477f1e68d37
Vihanga Bandara <vihangab@99x.lk>
2019-10-29 13:04:46 +0530

    Adding changes

--- src/Car.java
+++ src/Car.java
@@ -2,11 +2,13 @@
 public class Car extends Vehicle{
 	private int noOfDoors;
 	private String carColor;
+	private String carShade;
 	
-	public Car(String id, String brand, DateTime entryTime, int noOfDoors, String carColor) {
+	public Car(String id, String brand, DateTime entryTime, int noOfDoors, String carColor, string carShade) {
 		super(id, brand, entryTime);
 		this.noOfDoors = noOfDoors;
 		this.carColor = carColor;
+		this.carShade = carShade;
 	}
 	public int getNoOfDoors() {
 		return noOfDoors;
